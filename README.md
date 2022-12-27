13553d17a7a0fae0ef058a1e620e5e70a7bd5323
Maxim Makovskiy <makovskiyms@gmail.com>
2022-12-27 11:17:21 +0600

    Get ready for deployment

---
+++ README.md
@@ -0,0 +1,5 @@
+# restaurant-page
+
+This is a solution to the restaurant-page [project](https://www.theodinproject.com/lessons/node-path-javascript-restaurant-page) from the The Odin Project.
+
+The main goal of this project was to try out webpack and understand how does it work.
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
   },
   module: {
     rules: [
